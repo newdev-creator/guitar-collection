4bb18d1b6faa8662d1fb9947c64a4746a9081d1f
newdevcreator <newdevcreator@gmail.com>
2022-03-23 22:31:42 +0100

    HOMEPAGE__correction en cour

--- Doc/Integration/home/homeScript.js
+++ Doc/Integration/home/homeScript.js
@@ -1,5 +1,5 @@
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
+  const burger = document.querySelector('.nav-burger ');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
 
