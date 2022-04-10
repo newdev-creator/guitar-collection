b024459001331c22320d89026013f3ba821cb723
Newdevcreator <newdevcreator@gmail.com>
2022-04-10 17:50:38 +0200

    BACKOFFICE_post__controller, form created

---
+++ src/Controller/BackOffice/PostController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Controller\BackOffice;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+/**
+ * @Route("/backOffice/post", name="back_office_post")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="_browse", methods={"GET"})
+     */
+    public function browse(PostRepository $postRepository): Response
+    {
+        return $this->render('back_office/post/browse.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/read/{id}", name="_read", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function read(Post $post): Response
+    {
+
+        // $this->denyAccessUnlessGranted('POST_VIEW', $post);
+
+        return $this->render('back_office/post/read.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="_edit", methods={"GET", "POST"}, requirements={"id"="\d+"})
+     */
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+
+        // $this->denyAccessUnlessGranted('POST_EDIT', $post);
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                $post->setImage($newFilename);
+            }
+
+            $post->setUpdatedAt(new DateTimeImmutable());
+            $entityManager->flush();
+
+            $this->addFlash('success', "Le post à bien été modifiée");
+
+            // $postRepository->add($post);
+            return $this->redirectToRoute('back_office_post_browse', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back_office/post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="_add", methods={"GET", "POST"})
+     */
+    public function add(Request $request, PostRepository $postRepository, EntityManagerInterface $entityManager): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $postRepository->add($post);
+            return $this->redirectToRoute('back_office_post_browse', [], Response::HTTP_SEE_OTHER);
+        }
+
+        $entityManager->flush();
+        $this->addFlash('success', "Le post a été créée");
+
+        return $this->renderForm('back_office/post/add.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("delete/{id}", name="_delete", methods={"POST"}, requirements={"id"="\d+"})
+     */
+    public function delete(Request $request, Post $post, PostRepository $postRepository, EntityManagerInterface $entityManager): Response
+    {
+
+        // $this->denyAccessUnlessGranted('POST_DELETE', $post);
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $postRepository->remove($post);
+        }
+
+        $entityManager->flush();
+        $this->addFlash('success', "Le post a été supprimée");
+
+        return $this->redirectToRoute('back_office_post_browse', [], Response::HTTP_SEE_OTHER);
+    }
+}
